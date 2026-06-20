e24e2c20c66b3ae5b5fd9524fa74b6608dd4ba98
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-06-20 11:30:06 +0530

    c

---
+++ C/03_oprators/01_intro.c
@@ -0,0 +1,34 @@
+/*
+
+opatrots : opration 
+
+	1) assigement (=)
+	
+	2) artihmatic (+,-,*,/,%)
+
+	3) logical oprato (>,<,>=,<=,==,!=)
+	
+	4) rational opr 
+	
+		&& AND  : *
+		
+		T T = T 
+		T F = F
+		F T = F
+		F F = F
+		
+		|| OR
+		
+		T T = T
+		T F = T
+		F T = T 
+		F F = F
+		
+		! Not
+		
+		!true = false 
+		!false = true
+	
+	5) inc/dec ++,--
+		
+*/
