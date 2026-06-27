87e58a9d38d505b7e3a798cb5945bb1adbdd5a77
priya shah <priyashah.tops@gmail.com>
2026-06-27 11:29:50 +0530

    arra

---
+++ C/07_array/01_intro.c
@@ -0,0 +1,18 @@
+/*
+	
+	array , pointer,string 
+	
+	array : collection of multiple value store in single vaeribale .
+	
+	array [size]
+	array index = 0
+	
+	
+	syntax :
+	
+	int arr[size] = {2,4,324} 
+	
+	type :
+	1) 1d array 
+	2) 2d array 
+*/
