d078c179beb10bbbb6fe9bb452efe5eef7180660
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-07-08 11:25:09 +0530

    c++

---
+++ c++/02_Class_object/01_intro.cpp
@@ -0,0 +1,17 @@
+/*
+	
+	class: collecation of data memebr(varibale) and memebr function (user function) simple class .
+		 : it's blue print object
+		 
+		 data memebr 
+		 1) public : we can access 
+		 2) private : only class acces same class one
+		 3) protected : class accesss other class 
+		 
+	oop 
+	
+	1) class and object
+	2) inhritance
+	3) enc and abstr 
+	4) polimorization 
+*/
---
+++ c++/02_Class_object/03_class_ex.cpp
@@ -0,0 +1,37 @@
+
+#include<iostream>
+using namespace std;
+
+class Student{
+//	private:
+		int rollno;
+		string name;
+		
+	public:
+		void putData(){
+			cout<<"\nenter your Roll no :";
+			cin>>rollno;
+			cin.ignore();
+			cout<<"enter your Name :";
+			getline(cin, name);
+			
+		}
+		void getData(){
+			cout<<"Roll no : "<<rollno;
+			cout<<"\nName : "<<name;
+		}
+};
+
+int main()
+{
+	Student user;
+	Student s1;
+	
+	user.putData();
+	user.getData();
+	
+	s1.putData();
+	s1.getData();
+	
+	return 0;
+}
