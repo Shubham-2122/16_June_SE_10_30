b7b48ebaf058265f651d082bb822835d7b53e5f7
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-07-09 11:20:04 +0530

    c++

---
+++ c++/03_construtcor/01_intro.cpp
@@ -0,0 +1,16 @@
+ /*
+  It is a member function which initializes a class.
+• A constructor has:
+(i)the same name as the class itself
+(ii)no return type
+• A constructor is called automatically invoked whenever a new instance of a
+class is created.
+
+	type 3
+	
+	1) defualt 
+	2) parmater 
+	3) copy 
+	
+	4) deconstructor 
+ */
