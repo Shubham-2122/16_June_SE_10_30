4e381a70ea42d2c62aace9abd52907cfa1469f38
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-07-10 11:16:12 +0530

    c++

---
+++ c++/04_inheritance/01_intro.cpp
@@ -0,0 +1,10 @@
+/*
+	inhertiance : one class to another class 
+			    : proptey to property to data tranfer
+			    
+	1) singale : A - B 
+	2) multilevel : A- B - C 
+	3) multiple : A - B , C - B
+	4) hyrachical : A - B , A - C
+	5) hybrid : any two combine 
+*/
