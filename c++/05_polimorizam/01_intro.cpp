6e9e3d87f1032c83f0d66ce88887af30545ee0d4
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-07-11 11:22:50 +0530

    c++

---
+++ c++/05_polimorizam/01_intro.cpp
@@ -0,0 +1,6 @@
+/*
+	
+	1) method overloading : (comiple time) : class name ek and multiple function name same but defferent parameter
+	2) method overrding : (runtime error) : different class and funcation name same.
+	
+*/
---
+++ c++/06_enc_absc/01_intro.cpp
@@ -0,0 +1,11 @@
+/*
+	
+	enc  : Hide 
+	abscr : hide data access 
+	
+	class :
+	1) public : all access value
+	2) private : only one class access
+	3) protected : defferent class access ,inheriatnce accsess
+
+*/
